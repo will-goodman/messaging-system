07ab410a75d1d791a37e98b7650eed93b48cc7a2
Will Goodman <58141090+will-goodman@users.noreply.github.com>
2021-06-15 15:53:16 +0100

    Added Config class

---
+++ src/com/willgoodman/messagingsystem/Config.java
@@ -0,0 +1,32 @@
+package com.willgoodman.messagingsystem;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Stores common settings/functions required by both the Server and Client.
+ * Saves having to make changes in multiple classes.
+ *
+ * @author Will Goodman
+ * */
+public class Config {
+
+    public static final int port = 4444;
+    private static final int KEY_SIZE = 2048;
+    private static final String ENCRYPTION_ALGORITHM = "RSA";
+
+    /**
+     * Generates a private and public key pair for either the server or a client.
+     *
+     * @return a public key and its corresponding private key
+     * @throws NoSuchAlgorithmException if the chosen encryption algorithm is invalid
+     */
+    public static KeyPair generateKeys() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ENCRYPTION_ALGORITHM);
+        keyPairGenerator.initialize(KEY_SIZE);
+
+        return keyPairGenerator.genKeyPair();
+    }
+
+}
