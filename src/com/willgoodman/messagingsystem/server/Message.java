1997a34d2ce76d319474cd891dedcf02c62fff40
Will Goodman <58141090+will-goodman@users.noreply.github.com>
2021-06-18 22:59:05 +0100

    Added timestamp to Message to allow different messages with the same author and content to be compared

--- src/com/willgoodman/messagingsystem/server/Message.java
+++ src/com/willgoodman/messagingsystem/server/Message.java
@@ -1,24 +1,21 @@
 package com.willgoodman.messagingsystem.server;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Message {
 
-  private final String sender;
-  private final String text;
+  private final Timestamp TIMESTAMP;
+  private final String AUTHOR;
+  private final String TEXT;
 
   Message(String sender, String text) {
-    this.sender = sender;
-    this.text = text;
-  }
-
-  public String getSender() {
-    return sender;
-  }
-
-  public String getText() {
-    return text;
+    this.TIMESTAMP = new Timestamp(new Date().getTime());
+    this.AUTHOR = sender;
+    this.TEXT = text;
   }
 
   public String toString() {
-    return "From " + sender + ": " + text;
+    return "From " + this.AUTHOR + " at " + this.TIMESTAMP.toString() + ": " + this.TEXT;
   }
 }
