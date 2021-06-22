26964f5918aac171578cf15fe9f18ed991e76405
Will Goodman <58141090+will-goodman@users.noreply.github.com>
2021-06-22 19:46:06 +0100

    Added logging statements to Client

--- src/com/willgoodman/messagingsystem/client/Client.java
+++ src/com/willgoodman/messagingsystem/client/Client.java
@@ -11,6 +11,9 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Starts a fresh Client which connects to the server with the provided hostname
  *
@@ -18,52 +21,49 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 class Client {
 
-    private static final String USAGE = "Usage: java Client server-hostname";
-    //Error messages:
-    private static final String UNKNOWN_HOST = "Unknown host: ";
-    private static final String SERVER_NOT_RUNNING = "The server doesn't seem to be running ";
-    private static final String ALGORITHM_DOESNT_EXIST = "Encryption algorithm doesn't exist";
+    private static final Logger LOGGER = LogManager.getLogger(Client.class);
 
     public static void main(String[] args) {
-
-        // Check correct usage:
-        if (args.length != 1) {
-            Report.errorAndGiveUp(USAGE);
-        }
-
-        // Initialize information:
-        String hostname = args[0];
-
         try {
+            String hostname = args[0];
+            LOGGER.info(String.format("Hostname: %s", hostname));
+
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(Config.ENCRYPTION_ALGORITHM);
             keyPairGenerator.initialize(Config.KEY_SIZE);
             KeyPair keyPair = keyPairGenerator.genKeyPair();
+            LOGGER.debug("Generated KeyPair");
 
             Socket server = new Socket(hostname, Config.PORT);
             PrintStream toServer = new PrintStream(server.getOutputStream());
             BufferedReader fromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            LOGGER.debug("Connected to server");
 
             // Exchange public keys
             byte[] decodedServerKey = Base64.getDecoder().decode(fromServer.readLine());
             KeyFactory keyFactory = KeyFactory.getInstance(Config.ENCRYPTION_ALGORITHM);
             PublicKey serverPublicKey = keyFactory.generatePublic(new X509EncodedKeySpec(decodedServerKey));
             toServer.println(Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()));
+            LOGGER.debug("Exchanged keys");
 
             // when the user enters the quit command this triggers the ClientReceiver to close
             AtomicBoolean disconnect = new AtomicBoolean();
 
             (new ClientSender(toServer, serverPublicKey, disconnect)).start();
             (new ClientReceiver(fromServer, keyPair.getPrivate(), disconnect)).start();
+            LOGGER.debug("Started ClientSender and ClientReceiver threads");
 
+        } catch (NullPointerException ex) {
+            LOGGER.fatal("Usage: java Client server-hostname");
         } catch (NoSuchAlgorithmException ex) {
-            Report.errorAndGiveUp(ALGORITHM_DOESNT_EXIST);
+            LOGGER.fatal(String.format("Encryption algorithm %s is not available", Config.ENCRYPTION_ALGORITHM));
         } catch (UnknownHostException ex) {
-            Report.errorAndGiveUp(UNKNOWN_HOST + hostname);
+            LOGGER.fatal("Host could not be found");
         } catch (IOException ex) {
-            Report.errorAndGiveUp(SERVER_NOT_RUNNING + ex.getMessage());
+            LOGGER.fatal(String.format("The server may not be running: %s", ex.getMessage()));
         } catch (InvalidKeySpecException ex) {
-            Report.errorAndGiveUp("Error decoding server public key: " + ex.getMessage());
+            LOGGER.fatal(String.format("Error decoding server public key: %s", ex.getMessage()));
+        } finally {
+            System.exit(1);
         }
-
     }
 }
